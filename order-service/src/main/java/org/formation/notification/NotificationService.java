dae65ba7216022505d5ebbcd022c6700546b1255
David THIBAU <david.thibau@gmail.com>
2021-11-18 22:47:28 +0100

    SC LoadBalancer

--- order-service/src/main/java/org/formation/notification/NotificationService.java
+++ order-service/src/main/java/org/formation/notification/NotificationService.java
@@ -1,23 +1,21 @@
 package org.formation.notification;
 
 import org.formation.model.Client;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NotificationService {
 
+	@Autowired
 	RestTemplate restTemplate;
 
-	public NotificationService(RestTemplateBuilder builder) {
-		restTemplate = builder.build();
-	}
 
 	public String sendMail(Client client) {
 		Courriel c = Courriel.builder().to(client.getEmail()).text("Féliciations pour votre nouvelle commande")
 				.subject("Nouvelle commande").build();
-		return restTemplate.postForObject("http://notification-service.default.svc.cluster.local:8080/sendSimple", c,
+		return restTemplate.postForObject("http://notification-service/sendSimple", c,
 				String.class);
 	}
 }
