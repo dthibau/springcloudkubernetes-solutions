e6a6d93fd13cedce02d956bdcbce06292888cc91
David THI <david.thibau@gmail.com>
2021-12-14 19:12:02 +0100

    Canary deployment with Istio

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,5 @@
 kubectl apply -f notification-service/k8s/notification-prod.yaml
 kubectl apply -f delivery-service/k8s/delivery-service-prod.yaml
+kubectl apply -f delivery-service/k8s/delivery-service-prod-v2.yaml
 kubectl apply -f order-service/k8s/order-prod.yaml
 kubectl apply -f zipkin.yaml
